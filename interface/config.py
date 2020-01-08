ee6af27d42ca2a85dca8047b3f7abfc5a615ac24
therealpeterpython <47575127+therealpeterpython@users.noreply.github.com>
2020-01-08 23:36:35 +0100

    use more text files

--- interface/config.py
+++ interface/config.py
@@ -1,1 +1,8 @@
-token_path = "token.txt"
+log_path = "log.txt"        # log file
+token_path = "token.txt"    # file with the secret bot token
+help_path = "help.txt"      # file with the help text
+about_path = "about.txt"    # file with the about text
+expired_path = "expired.txt"  # file with the expired text
+sendall_path = "sendall.txt"  # file with the text for the send_all feature
+tmp_token_path = "tmp_token.txt"  # file with the temporary auth token to unlock admin features
+reports_path = "reports.txt"  # file to report illegal or failed attempts to use admin features

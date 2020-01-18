496f51515db156a41f8c9316619ba8252c6558b5
therealpeterpython <47575127+therealpeterpython@users.noreply.github.com>
2020-01-18 16:23:12 +0100

    updatet config

--- remembot/common/config.py
+++ remembot/common/config.py
@@ -1,9 +1,12 @@
-log_path = "logs/log.txt"             # log file
-reports_path = "logs/reports.txt"     # file to report illegal or failed attempts to use admin features
-tmp_token_path = "access/tmp_token.txt"  # file with the temporary auth token to unlock admin features
+log_path = "logs/log.txt"  # log file
+tasks_path = "tasks.pkl"   # tasks file
+#reports_path = "logs/reports.txt"  # file to report illegal or failed attempts to use admin features todo remove
+admins_path = "access/admins.txt"  # list of admin user-ids (not chat ids!)
+#tmp_token_path = "access/tmp_token.txt"  # file with the temporary auth token to unlock admin features todo remove
 token_path = "access/token.txt"          # file with the secret bot token
 sendall_path = "access/sendall.txt"      # file with the text for the send_all feature
 type_path = "../common/type.txt"      # file with the help text
 help_path = "../common/help.txt"      # file with the help text
 about_path = "../common/about.txt"    # file with the about text
 expired_path = "../common/expired.txt"  # file with the expired text
+

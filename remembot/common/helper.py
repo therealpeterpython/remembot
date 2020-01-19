5125ebca9b37fbce08d7b487e852517aad685405
therealpeterpython <47575127+therealpeterpython@users.noreply.github.com>
2020-01-20 00:30:36 +0100

    added documentation

--- remembot/common/helper.py
+++ remembot/common/helper.py
@@ -5,25 +5,30 @@ Helper functions
 from remembot.common.constants import *
 
 
-# splits the appointment string in blocks for the (maybe) multiple appointments
-# return none if its not a valid appointments string
-# Block formats:
-#  A;;ONCE;;13.09.2019;;14:45;;My text
-#  A;;EVERY_N_DAYS;;14;;13.09.2019;;14:45;;My text
-#  A;;NTH_WEEKDAY;;2;;MONDAY;;14:45;;My text
-#  A;;NUM;;13.09.2019;;14:45;;My text
 def parse_appointment_str(app_str):
+    """
+    Splits the appointment string in blocks for each appointment and
+    does a simpel checkup if they are valid.
+
+    The 4 different styles for appointment strings, if  DELIMITER = ;; and BLOCK_START = A, are:
+    A;;ONCE;;13.09.2019;;14:45;;My text
+    A;;EVERY_N_DAYS;;14;;13.09.2019;;14:45;;My text
+    A;;NTH_WEEKDAY;;2;;MONDAY;;14:45;;My text
+    A;;NUM;;13.09.2019;;14:45;;My text
+
+    :param app_str: String with all the information about the appointments
+    :return:
+    """
     print("-- parse_appointment_str")
     parameter = app_str.split(DELIMITER)
     index = [i for i, p in enumerate(parameter) if p == BLOCK_START]  # get the indices of the start token
     index.append(len(parameter))
     parameter_blocks = [parameter[index[i]: index[i+1]] for i in range(len(index)-1)]  # split into blocks
 
-    # check that each block has the right length (from this we conclude that the block is correct)
+    # check that each block has the correct length (from this we conclude that the blocks are valid)
     block_length = {ONCE: 5, EVERY_N_DAYS: 6, NTH_WEEKDAY: 6, NUM: 5}
     for block in parameter_blocks:
-        if block[1] not in ORDERS or block_length[block[1]] != len(block):
+        if block[1] not in block_length or block_length[block[1]] != len(block):
             return
 
-    print("parameter_blocks: ", parameter_blocks)
     return parameter_blocks

7067fe43ad7cbaaaea96f2bbb9b0ecb3558cc274
therealpeterpython <47575127+therealpeterpython@users.noreply.github.com>
2020-02-07 02:02:55 +0100

    made the main script robust against network errors

--- remembot/__main__.py
+++ remembot/__main__.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 from _thread import start_new_thread
+from telegram.error import NetworkError
 
 # add the top level package to the path to handle imports in subpackages #
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -13,13 +14,32 @@ from remembot.common.config import checkup_interval
 
 def appointment_checker():
     while True:
-        rememgram.check_tasks()  # check the tasks
-        time.sleep(checkup_interval)  # sleep
+        try:  # try to check the appointments
+            rememgram.check_tasks()
+        except NetworkError:  # ignore network errors
+            pass
+        time.sleep(checkup_interval)
 
 
 def main():
+    max_attempts = 30
+    sleep_time = 20
+
     start_new_thread(appointment_checker, ())  # start the appointment checker
-    frankenbot.main()  # start the bot (blocking)
+
+    # try to start the bot
+    for i in range(max_attempts):
+        try:
+            frankenbot.main()  # start the bot (blocking)
+            break
+        except NetworkError as e:
+            warning = str(e).split("/bot")  # split at /bot to censor private information about the bot
+            warning = warning[0] + "[...]" if len(warning) > 1 else warning[0]  # if there were such information replace them with [...]
+            print("Warning: ", warning)
+        time.sleep(sleep_time)
+    else:
+        raise NetworkError(warning)
+
 
     print("\n\nCiao, Kakao!")
 
